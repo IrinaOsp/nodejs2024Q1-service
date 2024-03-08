3cdd8b0edb84974302095832bf023949d3bf4ecf
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-03-08 16:35:00 +0100

    feat: add update track dto

--- src/track/dto/update-track.dto.ts
+++ src/track/dto/update-track.dto.ts
@@ -1,4 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTrackDto } from './create-track.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
-export class UpdateTrackDto extends PartialType(CreateTrackDto) {}
+export class UpdateTrackDto extends PartialType(CreateTrackDto) {
+  @ApiProperty({ required: false })
+  @IsString()
+  name: string;
+
+  @ApiProperty({ required: false })
+  @IsNumber()
+  duration: number;
+
+  @ApiProperty({ required: false })
+  albumId: string | null;
+
+  @ApiProperty({ required: false })
+  artistId: string | null;
+}

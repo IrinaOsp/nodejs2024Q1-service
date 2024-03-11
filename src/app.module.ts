3162aaff7b6f88d015ed164d3399c8e2042de42f
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-03-11 15:47:54 +0100

    feat: get port from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
@@ -9,13 +8,12 @@ import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UserModule,
     ArtistModule,
     TrackModule,
     AlbumModule,
     FavoritesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  const configService = app.get(ConfigService);
+  const port = parseInt(configService.get('PORT')) || 4000;
   const config = new DocumentBuilder()
     .setTitle('Home Library Service')
-    .setDescription('Home Library Service')
+    .setDescription('REST API for Home Library Service')
     .setVersion('1.0')
-    .addServer('http://localhost:4000')
+    .addServer(`http://localhost:${port}`)
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
 
-  await app.listen(4000);
+  await app.listen(port, () => {
+    console.log(`Application is running on: ${port}`);
+  });
 }
 bootstrap();
